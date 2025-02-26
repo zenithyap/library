dc51c4c9229ffc0df5056040a1e26023b7b95ccf
zenith <zenithyap88@gmail.com>
2025-02-26 17:28:31 +0800

    Add add book button functionality

--- library.js
+++ library.js
@@ -14,7 +14,7 @@ function addBookToLibrary(title, author, pages, read) {
 
 function display() {
     const library = document.querySelector(".card-container");
-    console.log(library);
+    library.
     for (const book of myLibrary) {
         const card = document.createElement("div");
         card.classList.add("card");
@@ -39,12 +39,22 @@ function display() {
     }
 }
 
-const dialog = document.querySelector("dialog");
+const dialog = document.querySelector("#add-book-dialog");
 const newBookButton = document.querySelector("#new-book-button");
-const closeButton = document.querySelector("form button");
+const addBookButton = document.querySelector("#add-book-button");
+const bookName = document.querySelector("#book-name");
+const bookAuthor = document.querySelector("#book-author");
+const bookPages = document.querySelector("#book-pages");
+const bookReadStatus = document.querySelector("#book-read-status");
 
 newBookButton.addEventListener("click", () => {
     dialog.showModal();
+});
+
+addBookButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    addBookToLibrary(bookName.value, bookAuthor.value, bookPages.value, bookReadStatus.value);
+    dialog.close();
 });
 
 addBookToLibrary("Behave", "Robert Sapsolky", "800", "reading");
@@ -56,18 +66,5 @@ addBookToLibrary("The Power of Habit", "Charles Duhigg", "371", "read");
 addBookToLibrary("The Subtle Art of Not Giving a F*ck", "Mark Manson", "224", "to read");
 addBookToLibrary("Deep Work", "Cal Newport", "296", "reading");
 addBookToLibrary("Meditations", "Marcus Aurelius", "254", "read");
-addBookToLibrary("The 48 Laws of Power", "Robert Greene", "452", "to read");
-addBookToLibrary("Grit", "Angela Duckworth", "352", "reading");
-addBookToLibrary("Outliers", "Malcolm Gladwell", "336", "read");
-addBookToLibrary("The Alchemist", "Paulo Coelho", "208", "to read");
-addBookToLibrary("Dune", "Frank Herbert", "688", "reading");
-addBookToLibrary("The Psychology of Money", "Morgan Housel", "256", "read");
-addBookToLibrary("The Four Agreements", "Don Miguel Ruiz", "160", "to read");
-addBookToLibrary("Can't Hurt Me", "David Goggins", "364", "reading");
-addBookToLibrary("Man's Search for Meaning", "Viktor E. Frankl", "200", "read");
-addBookToLibrary("Zero to One", "Peter Thiel", "224", "to read");
-addBookToLibrary("The Lean Startup", "Eric Ries", "336", "reading");
-addBookToLibrary("The Art of War", "Sun Tzu", "273", "read");
-addBookToLibrary("The Courage to Be Disliked", "Ichiro Kishimi, Fumitake Koga", "288", "to read");
 
 display();
